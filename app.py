31fbdbb4e11cd0bb1a35cfcec97e6586aec25f05
mharoon1578 <mharoon1578@gmail.com>
2025-09-01 17:21:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+def c_to_f(celsius):
+    return (celsius * 9/5) + 32
+
+def f_to_c(fahrenheit):
+    return (fahrenheit - 32) * 5/9
 
 def main():
     print("Welcome to Temperature Converter!")
+    print("25°C in Fahrenheit:", c_to_f(25))
+    print("77°F in Celsius:", f_to_c(77))
 
 if __name__ == "__main__":
     main()
